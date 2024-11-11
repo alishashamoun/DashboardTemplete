f39258ea76ebbf8840e420117577b555ee37c23c
alishashamoun <alishashamoun12345@gmail.com>
2024-11-11 17:43:33 +0500

    status file update

--- resources/views/bulk_status_update.blade.php
+++ resources/views/bulk_status_update.blade.php
@@ -16,9 +16,8 @@
 
 
                                                 <div class="row mb-3">
-                                                    <div class="col-4">
+                                                    <div class="col-5">
                                                         <div class="form-group">
-
                                                             <input type="text" class="form-control" name="from"
                                                                 placeholder="Pickup Run Sheet No">
                                                         </div>
@@ -35,28 +34,10 @@
 
 
                                                 <div class="row  mb-3">
-                                                    <div class="col-4">
+                                                    <div class="col-5">
                                                         <div class="form-group">
-
                                                             <input type="text" class="form-control" name="from"
-                                                                placeholder="Pickup Run Sheet No">
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="col-2 ">
-                                                        <div class="form-group">
-                                                            <input type="submit" name="submit" value="Pick Cn"
-                                                                class="btn btn-info">
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-                                                <div class="row  mb-3">
-                                                    <div class="col-4">
-                                                        <div class="form-group">
-
-                                                            <input type="text" class="form-control" name="from"
-                                                                placeholder="Pickup Run Sheet No">
+                                                                placeholder="Customer Run Sheet No">
                                                         </div>
                                                     </div>
 
@@ -70,11 +51,10 @@
 
 
                                                 <div class="row  mb-3">
-                                                    <div class="col-4">
+                                                    <div class="col-5">
                                                         <div class="form-group">
-
                                                             <input type="text" class="form-control" name="from"
-                                                                placeholder="Pickup Run Sheet No">
+                                                                placeholder="Delivery Run Sheet No">
                                                         </div>
                                                     </div>
 
@@ -87,29 +67,10 @@
                                                 </div>
 
                                                 <div class="row  mb-3">
-                                                    <div class="col-4">
+                                                    <div class="col-5">
                                                         <div class="form-group">
-
                                                             <input type="text" class="form-control" name="from"
-                                                                placeholder="Pickup Run Sheet No">
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="col-2 ">
-                                                        <div class="form-group">
-                                                            <input type="submit" name="submit" value="Pick Cn"
-                                                                class="btn btn-info">
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-
-                                                <div class="row  mb-3">
-                                                    <div class="col-4">
-                                                        <div class="form-group">
-
-                                                            <input type="text" class="form-control" name="from"
-                                                                placeholder="Pickup Run Sheet No">
+                                                                placeholder="returnSheet No">
                                                         </div>
                                                     </div>
 
@@ -123,11 +84,27 @@
 
 
                                                 <div class="row  mb-3">
-                                                    <div class="col-4">
+                                                    <div class="col-5">
                                                         <div class="form-group">
-
                                                             <input type="text" class="form-control" name="from"
-                                                                placeholder="Pickup Run Sheet No">
+                                                                placeholder="Manifest No">
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="col-2 ">
+                                                        <div class="form-group">
+                                                            <input type="submit" name="submit" value="Pick Cn"
+                                                                class="btn btn-info">
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+
+                                                <div class="row  mb-3">
+                                                    <div class="col-5">
+                                                        <div class="form-group">
+                                                            <input type="text" class="form-control" name="from"
+                                                                placeholder="De-Manifest No">
                                                         </div>
                                                     </div>
 
@@ -180,11 +157,8 @@
                                                             Select Statuses (*)</label>
                                                         <select name="" id=""
                                                             class="js-example-templating form-control">
-                                                            <option value="">new booked</option>
-                                                            <option value="">new booked</option>
-                                                            <option value="">parcel</option>
-                                                            <option value="">parcel</option>
-
+                                                            <option value=""></option>
+                                                            <option value=""></option>
                                                         </select>
                                                     </div>
                                                 </div>
@@ -200,8 +174,8 @@
                             </div>
                         </div>
                     </div>
-                    <div class="row mt-5">
-                        <div class="col-2">
+                    <div class="row d-flex justify-content-end mt-5">
+                        <div class="col-4">
                             <div class="form-group">
 
                                 <input type="text" class="form-control" name="from" placeholder="Enter CN No">
@@ -213,82 +187,91 @@
                                 <input type="submit" name="submit" value="Submit" class="btn btn-info">
                             </div>
                         </div>
-                    </div>
 
-                    <div class="card mt-5">
-                        <div class="card-body">
-                            <table class="table table-stripe">
-                                <tr>
-                                    <th>CN#</th>
-                                    <th>Service Type</th>
-                                    <th>Shipper</th>
-                                    <th>Origin</th>
-                                    <th>Consignee</th>
-                                    <th>Destination</th>
-                                    <th>Status</th>
-                                    <th>PCS</th>
-                                    <th>Weight</th>
-                                    <th>Action</th>
-                                </tr>
-                            </table>
+                        <div class="col-4">
+                            <div class="form-group">
+
+                                <input type="text" class="form-control" name="from" placeholder="Enter Weight">
+                            </div>
+                        </div>
+
+                        <div class="col-2">
+                            <div class="form-group">
+                                <input type="submit" name="submit" value="Update" class="btn btn-info">
+                            </div>
                         </div>
                     </div>
+                </div>
 
-                    <div class="card mt-5">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-sm-9">
-                                    <label>Remarks</label>
-                                    <textarea name="" id="" cols="100" rows="8" class="form-control"></textarea>
-                                </div>
-                                <div class="col-sm-3">
-                                    <div class="row">
-                                        <div class="col-6">
-                                            <label for="">PCS</label>
-                                            <input type="text" value="00" class="form-control">
-                                        </div>
-                                        <div class="col-6">
-                                            <label for="">Weight</label>
-                                            <input type="text" value="00" class="form-control">
-                                        </div>
+                <div class="card mt-5">
+                    <div class="card-body">
+                        <table class="table table-stripe">
+                            <tr>
+                                <th>CN#</th>
+                                <th>Service Type</th>
+                                <th>Shipper</th>
+                                <th>Origin</th>
+                                <th>Consignee</th>
+                                <th>Destination</th>
+                                <th>Status</th>
+                                <th>PCS</th>
+                                <th>Weight</th>
+                                <th>Action</th>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+
+                <div class="card mt-5">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-sm-9">
+                                <label>Remarks</label>
+                                <textarea name="" id="" cols="100" rows="8" class="form-control"></textarea>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <label for="">PCS</label>
+                                        <input type="text" value="00" class="form-control">
                                     </div>
-                                    <div class="row mt-2">
-                                        <div class="col-12">
-                                            <label for="">Order Update Status (*)</label>
-                                            <select name="" id=""
-                                                class="js-example-templating form-control">
-                                                <option value="">new booked</option>
-                                                <option value="">new booked</option>
-                                                <option value="">parcel</option>
-                                                <option value="">parcel</option>
-                                                <option value="">parcel</option>
-                                                <option value="">parcel</option>
-                                                <option value="">parcel</option>
-                                                <option value="">parcel</option>
-
-                                            </select>
-                                        </div>
+                                    <div class="col-6">
+                                        <label for="">Weight</label>
+                                        <input type="text" value="00" class="form-control">
                                     </div>
                                 </div>
                                 <div class="row mt-2">
-                                    <div class="col-3">
-                                        <label for="">Status Date</label>
-                                        <input type="date" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="row mt-3">
-                                    <div class="col-3">
-                                        <button class="btn btn-info">Update Status</button>
-                                    </div>
-                                    <div class="col-3">
-                                        <button class="btn btn-dark">collect Cash</button>
-                                    </div>
-                                </div>
+                                    <div class="col-12">
+                                        <label for="">Order Update Status (*)</label>
+                                        <select name="" id="" class="js-example-templating form-control">
+                                            <option value="">new booked</option>
+                                            <option value="">new booked</option>
+                                            <option value="">parcel</option>
+                                            <option value="">parcel</option>
+                                            <option value="">parcel</option>
+                                            <option value="">parcel</option>
+                                            <option value="">parcel</option>
+                                            <option value="">parcel</option>
 
+                                        </select>
+                                    </div>
+                                </div>
                             </div>
-
+                            <div class="row mt-2">
+                                <div class="col-3">
+                                    <label for="">Status Date</label>
+                                    <input type="date" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-3">
+                                    <button class="btn btn-info">Update Status</button>
+                                </div>
+                                <div class="col-3">
+                                    <button class="btn btn-dark">collect Cash</button>
+                                </div>
+                            </div>
                         </div>
-
                     </div>
                 </div>
             </div>
